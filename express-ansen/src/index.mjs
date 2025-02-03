949907b20a545a7e1e59ac9d0c904abf3a10c32d
dare-tunmise <daretunmise@yahoo.com>
2025-02-03 15:24:21 +0100

    ...

--- express-ansen/src/index.mjs
+++ express-ansen/src/index.mjs
@@ -4,24 +4,48 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [
+    {id: 1, username: "Dare", displayName: "dare_tunmise" },
+    {id: 2, username: "bolu", displayName: "bolu_taiwo"},
+    {id: 3, username: "damilola", displayName: "damilola_dare"}
+]
+
+const mockProducts = [
+    {id: 123, name: "gizzard", price: 2500 },
+    {id: 234, name: "carrot", price: 500 },
+    {id: 347, name: "onions", price: 800 }
+]
+
 app.get('/', (request, response)=> {
     response.status(201).send({ msg: "Server Says Hello World!"});
 })
 
 app.get('/api/users', (request, response) => {
-    response.send([
-        {id: 1, username: "Dare", displayName: "dare_tunmise" },
-        {id: 2, username: "bolu", displayName: "bolu_taiwo"},
-        {id: 1, username: "damilola", displayName: "damilola_dare"}
-    ])
+    response.send(mockUsers);
 });
 
+app.get('/api/users/:id', (request, response)=> {
+    console.log(request.params);
+    const parseId = parseInt(request.params.id);
+    console.log(parseId)
+    if(isNaN(parseId)) return response.status(400).send({ msg: "Bad Request. Invalid ID"});
+
+    const findUser = mockUsers.find((user)=> user.id === parseId);
+    if(!findUser) return response.sendStatus(400);
+    return response.send(findUser);
+})
+
 app.get('/api/products', (request, response)=> {
-    response.send([
-        {id: 123, name: "gizzard", price: 2500 },
-        {id: 234, name: "carrot", price: 500 },
-        {id: 347, name: "onions", price: 800 }
-    ])
+    response.send(mockProducts)
+})
+
+app.get('/api/products/:id', (request, response)=> {
+    const parseId = parseInt(request.params.id);
+    if(isNaN(parseId)) return response.status(400).send({msg: "not a valid id, nigga!"});
+
+    const findProduct = mockProducts.find((product)=> product.id === parseId );
+    if(!findProduct) return response.sendStatus(400);
+    return response.send(findProduct)
 })
 
 app.listen(PORT, ()=> {
