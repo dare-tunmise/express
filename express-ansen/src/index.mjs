4ec11f785e6434ccc32a3a6b8ca6a8b3ea5337bf
dare-tunmise <daretunmise@yahoo.com>
2025-02-03 14:42:51 +0100

    ...

--- express-ansen/src/index.mjs
+++ express-ansen/src/index.mjs
@@ -1,10 +1,28 @@
-import express, { json } from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
-app.use(json())
-
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (request, response)=> {
+    response.status(201).send({ msg: "Server Says Hello World!"});
+})
+
+app.get('/api/users', (request, response) => {
+    response.send([
+        {id: 1, username: "Dare", displayName: "dare_tunmise" },
+        {id: 2, username: "bolu", displayName: "bolu_taiwo"},
+        {id: 1, username: "damilola", displayName: "damilola_dare"}
+    ])
+});
+
+app.get('/api/products', (request, response)=> {
+    response.send([
+        {id: 123, name: "gizzard", price: 2500 },
+        {id: 234, name: "carrot", price: 500 },
+        {id: 347, name: "onions", price: 800 }
+    ])
+})
 
 app.listen(PORT, ()=> {
     console.log(`Running on Port ${PORT}`);
